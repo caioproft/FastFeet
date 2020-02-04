50b3ee076bf1442dd33f5d6025fdf3d97b953241
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-02-04 08:29:50 -0300

    Configuraçã do ESLint, Prettier e EditorConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  plugins: ['prettier'
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
   },
 };
