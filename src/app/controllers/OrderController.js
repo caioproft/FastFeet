cc5515d0098809dcbb3837855f4ae5241e121830
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-02-19 15:14:24 -0300

    Criação dos métodos update e delete no OrderController

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -7,8 +7,8 @@ import File from '../models/File';
 class OrderController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      recipient_id: Yup.number().integer(),
-      deliveryman_id: Yup.number().integer(),
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
       product: Yup.string().required(),
     });
 
@@ -77,6 +77,74 @@ class OrderController {
     });
 
     return res.json(orders);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      signature_id: Yup.number(),
+      product: Yup.string(),
+      canceled_at: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'É preciso informar o destinatário, entregador e produto.',
+      });
+    }
+
+    const { id } = req.params;
+    const { recipient_id, deliveryman_id } = req.body;
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res
+        .status(400)
+        .json({ error: 'Não há encomenda cadastrada com esse ID.' });
+    }
+
+    if (recipient_id && recipient_id !== order.recipient_id) {
+      const checkRecipientExists = await Recipient.findByPk(recipient_id);
+
+      if (!checkRecipientExists) {
+        return res.status(400).json({
+          error: 'Não existe um destinatário cadastrado com esse ID.',
+        });
+      }
+    }
+
+    if (deliveryman_id && deliveryman_id !== order.deliveryman_id) {
+      const checkDeliverymanExists = await Deliveryman.findByPk(deliveryman_id);
+
+      if (!checkDeliverymanExists) {
+        return res
+          .status(400)
+          .json({ error: 'Não existe um entregador cadastrado com esse ID.' });
+      }
+    }
+
+    await order.update(req.body);
+
+    return res.json(order);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res
+        .status(400)
+        .json({ error: 'Não existe encomenda cadastrada com esse ID.' });
+    }
+
+    order.canceled_at = new Date();
+
+    await order.save();
+
+    return res.json(order);
   }
 }
 
